5735389b5787d48fdfafb3b2a01771ad059ad819
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-05-16 14:01:27 +0530

    Create main.tf

---
+++ Infra/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "kms" {
+  source = "./modules/kms"
+}
+
+module "ssm" {
+  source          = "./modules/ssm"
+  kms_key_id      = module.kms.kms_key_arn
+  parameter_name  = "/myapp/db-password"
+  parameter_value = "SuperSecret123!"
+}
+
+module "sns" {
+  source = "./modules/sns"
+  email  = "jorepalli.vamsee@hcltech.com"
+}
+
+module "eventbridge" {
+  source        = "./modules/eventbridge"
+  sns_topic_arn = module.sns.sns_topic_arn
+}
+
+module "lambda" {
+  source      = "./modules/lambda"
+  kms_key_arn = module.kms.kms_key_arn
+}
