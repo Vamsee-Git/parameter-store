9c91aa5055d70ecbe50cad2bd8a81c7b8499cc89
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-05-16 14:47:46 +0530

    Update main.tf

--- Infra/modules/lambda/main.tf
+++ Infra/modules/lambda/main.tf
@@ -51,7 +51,7 @@ resource "aws_lambda_function" "read_ssm" {
   filename         = "lambda_function_payload.zip"
   function_name    = "ReadSSMParameter"
   role             = aws_iam_role.lambda_role.arn
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.9"
+  handler          = "lambda_function_payload.lambda_handler"
+  runtime          = "python3.10"
   source_code_hash = filebase64sha256("lambda_function_payload.zip")
 }
